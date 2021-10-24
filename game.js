61bcf33a7e31cb84f943c13b9224956ab336b47a
MOHD_VAIS <mohammadvais2@gmail.com>
2021-10-24 17:25:48 +0530

    second commit

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
+var buttonColours =['red','blue','green','yellow'];
+
+var gamePattern =[];
+
+
+
+var 
+
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 }
